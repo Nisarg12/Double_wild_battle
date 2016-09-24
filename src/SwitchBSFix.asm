4c9c689956822446e7ff087e36d61b4eef31d3da
jiangzhengwenjz <jiangzhengwenjzw@qq.com>
2016-09-24 23:29:33 +0800

    Switch hack improved

--- src/SwitchBSFix.asm
+++ src/SwitchBSFix.asm
@@ -6,25 +6,10 @@ jumpifword 0 0x2022B4C 5 case_double
 goto 0x81D86BB
 
 case_double:
-callasm check_validity+1
-jumpifhalfword 0 0x20370C0 1 case_no
+jumpifcannotswitch 0xB case_no
 goto 0x81D86BB
 
 case_no:
 openpartyscreen 0x3 0x81D87B7
 cmd51 0x3 0x2
 goto 0x81D8792
-
-
-.align 2
-check_validity:
-push {lr}
-mov r0, #6
-ldr r1, =0x808C185
-bl bx_r1
-ldr r1, =0x20370C0
-strh r0, [r1]
-pop {pc}
-.ltorg
-
-bx_r1: bx r1
